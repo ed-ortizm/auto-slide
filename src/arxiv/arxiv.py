1672725b31694c61a4d78b1a1f065d9344c5bf56
Edgar Ortiz <ed.ortizm@gmail.com>
2021-09-24 16:51:45 -0300

    arxiv module set up

---
+++ src/arxiv/arxiv.py
@@ -0,0 +1,13 @@
+from pptx import Presentation
+from pptx.util import Inches
+
+
+def add_image(
+    presentation:'pptx.Presentation',
+    location:'str',
+    names:'list',
+    format:'str'=png
+):
+    pass
+
+    return presentation

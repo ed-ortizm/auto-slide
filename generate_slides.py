13cc0af4e56ae9a50c47afe3f21d60b96eef4842
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-07-11 12:16:20 -0400

    update code

--- generate_slides.py
+++ generate_slides.py
@@ -1,21 +1,20 @@
-from configparser import ConfigParser, ExtendedInterpolation
-import glob
+"""Test pptx module"""
+# import glob
 import time
-
-import numpy as np
+from configparser import ConfigParser, ExtendedInterpolation
 
 from pptx import Presentation
+from pptx.dml.color import RGBColor
+# from pptx.enum.dml import MSO_THEME_COLOR
 from pptx.enum.shapes import MSO_SHAPE
 from pptx.util import Inches
-from pptx.enum.dml import MSO_THEME_COLOR
-from pptx.dml.color import RGBColor
+
 ###############################################################################
 start_time = time.time()
 ###############################################################################
 parser = ConfigParser(interpolation=ExtendedInterpolation())
-config_file_name = "generate_slides.ini"
-parser.read(f"{config_file_name}")
-
+CONFIG_FILE_NAME = "generate_slides.ini"
+parser.read(f"{CONFIG_FILE_NAME}")
 ###############################################################################
 presentation_name = parser.get("file", "presentation_name")
 presentation = Presentation()
@@ -23,13 +22,12 @@ presentation = Presentation()
 TITLE_AND_CONTENT = 1
 BLANK = 6
 ###############################################################################
-print(f"Set title", end="\n")
+print("Set title", end="\n")
 
 layout = presentation.slide_layouts[BLANK]
 slide = presentation.slides.add_slide(layout)
 ###############################################################################
 shapes = slide.shapes
-
 left = Inches(4.0)
 top = Inches(4.0)
 width = Inches(3.0)
@@ -45,7 +43,7 @@ shape = shapes.add_shape(
 fill = shape.fill
 fill.solid()
 fill.fore_color.rgb = RGBColor(255, 0, 0)
-
+a = [(1, 1), [3, 5], [(3, 5), 4]]
 ###############################################################################
 line = shape.line
 line.color.rgb = RGBColor(255, 0, 0)
